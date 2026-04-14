ffe7c139858e9a440ad8151630a38d80f2cd2527
marrrkkk <definitelynotmark13@gmail.com>
2026-04-14 20:39:01 +0800

    test: add DOM API mocks for component testing

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,27 @@
 import '@testing-library/jest-dom';
+
+global.ResizeObserver = class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+// Also mock PointerEvent since it's sometimes needed for Radix UI components
+if (typeof global.PointerEvent === 'undefined') {
+  class PointerEvent extends MouseEvent {
+    public pointerId: number;
+    public pointerType: string;
+    public isPrimary: boolean;
+
+    constructor(type: string, params: PointerEventInit = {}) {
+      super(type, params);
+      this.pointerId = params.pointerId ?? 0;
+      this.pointerType = params.pointerType ?? '';
+      this.isPrimary = params.isPrimary ?? false;
+    }
+  }
+  global.PointerEvent = PointerEvent as any;
+}
+
+// Mock window.HTMLElement.prototype.scrollIntoView
+window.HTMLElement.prototype.scrollIntoView = function() {};
