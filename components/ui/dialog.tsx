1237f88272c39273ea7f43f0950188af0e3dd570
marrrkkk <definitelynotmark13@gmail.com>
2026-04-18 01:48:08 +0800

    refactor: update dialog and sheet overlay styling

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -45,7 +45,7 @@ function DialogOverlay({
     <DialogPrimitive.Overlay
       data-slot="dialog-overlay"
       className={cn(
-        "fixed inset-0 z-50 bg-black/32 duration-100 data-open:animate-in data-open:fade-in-0 data-closed:animate-out data-closed:fade-out-0 supports-backdrop-filter:backdrop-blur-[2px]",
+        "fixed inset-0 z-50 bg-black/32 duration-100 data-open:animate-in data-open:fade-in-0 data-closed:animate-out data-closed:fade-out-0",
         className
       )}
       {...props}
